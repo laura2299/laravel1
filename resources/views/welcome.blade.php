d57c108739dda09cfca8d1d0086ffa0726d821c6
laura22 <lpatzit2@fcpn.edu.bo>
2023-05-13 08:44:32 +0400

    laravel

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>Coming Soon Evening</title>
+        <link href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet">
+        <link rel="stylesheet" href="css/bootstrap.min.css">
+        <link rel="stylesheet" href="css/jquery.countdown.css">
+        <link rel="stylesheet" href="css/font-awesome.min.css">
+        <link rel="stylesheet" href="style.css">
+    </head>
+    <body>
+      <div class="wrapper">
+           <div class="opacity"></div>
+            <div class="gravity">
+               <div class="container">
+                   <div class=" row">
+                       <div class="col-md-12">
+                       <img class="logo" src="img/logoselin2.png" alt="Gravity" width="300" height="150" align="center">
+                       <P>Nuestro sitio Web llegará pronto</P>
+                       </div>
+                   </div>
+               </div>
+            </div>
+            <div id="countdown"></div>
+    
+            <div class="email">
+               <div class="container">
+                   <div class=" row">
+                       <div class="col-md-12">
+                           <p>SITIO EN CONSTRUCCION</p>
+                           
+                           
+                       </div>
+                   </div>
+               </div>
+            </div>
+    
+            <div class="social-icon">
+                <div class="container">
+                    <div class=" row">
+                       <div class="col-md-12">
+                            <div class="icon">
+                               <a href="https://www.facebook.com/seelin.ingenieria/"><i class="fa fa-facebook"></i></a>
+                               
+                               <a href="mailto:info@seelin.bo?subject=Preguntas y dudas"><i class="fa fa-google-plus"></i></a>
+                            </div>
+    
+                       </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    	
+		<script src="js/jquery-3.1.1.min.js"></script>
+        <script src="js/bootstrap.min.js"></script>
+        <script src="js/jquery.countdown.js"></script>
+        <script src="js/active.js"></script>
+    
+    
+    </body>
+
+
+</html>
